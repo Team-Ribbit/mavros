397414f1fb917768454b45db74db9617397de111
Justin-Ribbit <justin.tomasi@flyribbit.com>
2023-10-17 15:53:31 -0600

    Fix for the network_unreachable bug.

--- libmavconn/src/udp.cpp
+++ libmavconn/src/udp.cpp
@@ -283,7 +283,8 @@ void MAVConnUDP::do_sendto(bool check_tx_state)
 
 				if (error == boost::asio::error::network_unreachable) {
 					CONSOLE_BRIDGE_logWarn(PFXd "sendto: %s, retrying", sthis->conn_id, error.message().c_str());
-					// do not return, try to resend
+					sthis->close();
+					return;
 				}
 				else if (error) {
 					CONSOLE_BRIDGE_logError(PFXd "sendto: %s", sthis->conn_id, error.message().c_str());
